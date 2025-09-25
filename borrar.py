de1caa111f773333d8f42e6c4894addac838c757
astiazu <astiazu@gmail.comm>
2025-09-25 16:43:38 -0300

    Temp: restaurar rutas

--- borrar.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-with app.app_context():
-    # Elimina la tabla alembic_version si existe
-    db.engine.execute("DROP TABLE IF EXISTS alembic_version;")
-    print("✅ Tabla alembic_version eliminada")
