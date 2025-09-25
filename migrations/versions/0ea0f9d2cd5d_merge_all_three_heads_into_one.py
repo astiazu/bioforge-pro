8721b55e43d7dd255195d22d37b40fa43a390c26
astiazu <astiazu@gmail.comm>
2025-09-25 08:29:56 -0300

    Fix: merge all three heads into one

---
+++ migrations/versions/0ea0f9d2cd5d_merge_all_three_heads_into_one.py
@@ -0,0 +1,24 @@
+"""Merge all three heads into one
+
+Revision ID: 0ea0f9d2cd5d
+Revises: 
+Create Date: 2025-09-25 08:29:25.534521
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0ea0f9d2cd5d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
